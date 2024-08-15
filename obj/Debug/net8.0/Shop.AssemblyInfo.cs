35dabd6550c5f3f54f866697678fc35909a62da4
InacioCarvalhoOliveira <inacio.carvalho.oliveira02@gmail.com>
2024-08-15 20:57:03 -0300

    ok

--- obj/Debug/net8.0/Shop.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7b88eb05b87eff7c24091962e85e6028588925")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e6d5b8c0b48854c49f30431e71035cb11f18537")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/Shop.AssemblyInfo.cs
+++ obj/Release/net8.0/Shop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b31ce44474c385dfd90c0071b52cf728020947c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e6d5b8c0b48854c49f30431e71035cb11f18537")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

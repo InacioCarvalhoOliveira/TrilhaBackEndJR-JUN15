bb04577bfa45991ee206079d268ee77e4376f32f
InacioCarvalhoOliveira <inacio.carvalho.oliveira02@gmail.com>
2024-09-24 20:53:10 -0300

    bulild: rota da api

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Shop;
@@ -6,19 +8,39 @@ using Shop.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure Kestrel
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Limits.MinRequestBodyDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(15));
+    options.Limits.MinResponseDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(15));
+    options.Limits.MaxRequestBodySize = 104857600; // 100 MB, set to null for unlimited
+});
+
 // Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "Shop", Version = "v1" });
 });
-//builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
-//garante que o DataContext seja instanciado uma vez por requisição
+
+//add cors
+builder.Services.AddCors(options =>
+{
+     options.AddPolicy("AllowAllOrigins",
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:5039") // Adjust to the origin of your frontend
+                           .AllowAnyMethod()
+                           .AllowAnyHeader()
+                           .AllowCredentials(); // If you need credentials (like cookies)
+                });
+});
+
 builder.Services.AddScoped<DataContext, DataContext>();
 builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("connectionString")));
-var key = System.Text.Encoding.ASCII.GetBytes(Settings.Secret);
+
+var key = Encoding.ASCII.GetBytes(Settings.Secret);
 builder.Services.AddAuthentication(x =>
 {
     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -31,29 +53,35 @@ builder.Services.AddAuthentication(x =>
     {
         ValidateIssuerSigningKey = true,
         IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
+        ValidateIssuer = true,
+        ValidateAudience = true
     };
 });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//}
-
-app.UseDeveloperExceptionPage();
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 app.UseSwagger();
-
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop"));
 
+//use cors
+app.UseCors("AllowAllOrigins");
 app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run("http://*:5005"); // Configure the application to listen on port 5039
